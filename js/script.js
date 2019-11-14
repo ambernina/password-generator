47aa5d9f0c98f7d058ce0abe58df2b451ea70940
Ambernina <rob.ambernina@gmail.com>
2019-11-13 18:28:56 -0700

    updated code fixing the final array after tutor helped

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) {
 var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numArr = [1,2,3,4,5,6,7,8,9,0];
@@ -5,58 +6,66 @@ var specCharArr = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "="];
 
 var result = document.getElementById("result");
 
+var userGeneratedPassword = "";
+var userPassArr = [];
+var length ;
 
 function prompts() {
-    var length = prompt("How long would you like your password to be between 8 and 128 characters?");
+    userGeneratedPassword = "";
+    userPassArr = [];
+    var lengthTemp = prompt("How long would you like your password to be between 8 and 128 characters?");
     var lowerPrompt = confirm("Would you like to include lower case letters?");
     var upperPrompt = confirm("Would you like to include upper case letters?");
     var numPrompt = confirm("Would you like to include numbers?");
     var specCharPrompt = confirm("Would you like to include special characters?");
     
-    length = parseInt(length);
-    var userPassArr = [];
+    length = parseInt(lengthTemp);
     
-    if (lowerPrompt === true) {
-        userPassArr.push(lowerCaseArr);
+    if (lowerPrompt) {
+        userPassArr = userPassArr.concat(lowerCaseArr);
     }
-    if (upperPrompt === true) {
-        userPassArr.push(upperCaseArr);
+    if (upperPrompt) {
+        userPassArr = userPassArr.concat(upperCaseArr);
     }
-    if (numPrompt === true) {
-        userPassArr.push(numArr);
+    if (numPrompt) {
+        userPassArr = userPassArr.concat(numArr);
     }
-    if (specCharPrompt === true) {
-        userPassArr.push(specCharArr);
+    if (specCharPrompt) {
+        userPassArr = userPassArr.concat(specCharArr);
     }
-
-    return {
-        length: length,
-        passArr: userPassArr.flat()
-    }
-
 }
 
-var passValues = prompts();
-console.log(passValues);
 
-
-function password(length, specifiedCharacters) {
+function password() {
     
-    for (var i = 0; i < length.value; i++) {
-        userGeneratedPassword += passValues.charAt(Math.floor(Math.random() * passValues.length));
+    for (var i = 0; i < length; i++) {
+        userGeneratedPassword += userPassArr[(Math.floor(Math.random() * userPassArr.length))];
         // userGeneratedPassword.push(passValues[i]);
         console.log(userGeneratedPassword);
         result.textContent = userGeneratedPassword;
     }
-    
     // var userGeneratedPassword = [];
 }
 
-password(passValues.length, passValues.passArr);
-console.log(password);
+prompts()
+password();
+// document.getElementsById("one").addEventListener("click", function(){
+//     console.log("YOU CLICKED IT")
+//     prompts()
+//     })
+//     })
 
-
-
+document.getElementById("two").addEventListener("click", function(){
+console.log("YOU CLICKED IT")
+var text = document.getElementById("result").innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+})
+})
 
 
 
